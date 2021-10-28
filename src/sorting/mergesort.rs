798488dbb133b6d6d11fe671f89f636dee9d39ed
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-10-28 22:20:16 +0200

    Improvements to mergesort.

--- src/sorting/mergesort.rs
+++ src/sorting/mergesort.rs
@@ -16,25 +16,30 @@ fn merge(seq: &mut [i32], split: usize) {
     let mut left_idx = 0;
     let mut right_idx = split;
 
-    while left_idx < split || right_idx < seq.len() {
-        if left_idx < split && right_idx < seq.len() {
-            if buffer[left_idx] <= buffer[right_idx] {
-                seq[write_idx] = buffer[left_idx];
-                left_idx += 1;
-                write_idx += 1;
-            } else {
-                seq[write_idx] = buffer[right_idx];
-                right_idx += 1;
-                write_idx += 1;
-            }
-        } else if left_idx < split {
-            seq[write_idx] = buffer[left_idx];
+    while left_idx < split && right_idx < seq.len() {
+        let left_val = buffer[left_idx];
+        let right_val = buffer[right_idx];
+
+        if left_val <= right_val {
+            seq[write_idx] = left_val;
             left_idx += 1;
-            write_idx += 1;
         } else {
-            seq[write_idx] = buffer[right_idx];
+            seq[write_idx] = right_val;
             right_idx += 1;
-            write_idx += 1;
         }
+
+        write_idx += 1;
+    }
+
+    while left_idx < split {
+        seq[write_idx] = buffer[left_idx];
+        left_idx += 1;
+        write_idx += 1;
+    }
+
+    while right_idx < seq.len() {
+        seq[write_idx] = buffer[right_idx];
+        right_idx += 1;
+        write_idx += 1;
     }
 }
