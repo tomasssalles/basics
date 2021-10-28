64459ae2e0acf8e6f78313420fcae62227222bd9
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-10-28 20:52:25 +0200

    Implemented quicksort.

--- src/sorting/quicksort.rs
+++ src/sorting/quicksort.rs
@@ -1,1 +1,26 @@
-pub fn quicksort(seq: &mut [i32]) {}
+pub fn quicksort(seq: &mut [i32]) {
+    if seq.len() < 2 {
+        return;
+    }
+
+    let mut pivot_idx = 0;
+    let pivot_val = seq[0];
+
+    for idx in 1 .. seq.len() {
+        let val = seq[idx];
+
+        if val < pivot_val {
+            seq[pivot_idx] = val;
+
+            pivot_idx += 1;
+
+            if idx > pivot_idx {
+                seq[idx] = seq[pivot_idx];
+            }
+        }
+    }
+
+    seq[pivot_idx] = pivot_val;
+    quicksort(&mut seq[..pivot_idx]);
+    quicksort(&mut seq[(pivot_idx+1)..]);
+}
