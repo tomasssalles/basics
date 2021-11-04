288fdf9f12a05f0eee96c4115300dfad1bd88849
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-11-04 16:05:41 +0100

    Done with heapify.

--- src/sorting/heap.rs
+++ src/sorting/heap.rs
@@ -1,5 +1,38 @@
 pub fn max_heapify(seq: &mut [i32]) {
-    if seq.len() >= 2 && seq[0] < seq[1] {
-        seq.swap(0, 1);
+    let n = seq.len();
+
+    if n < 2 {
+        return;
+    }
+
+    let start = 0;
+    let root = seq[start];
+    let mut idx = start;
+
+    loop {
+        let l_child = (2*idx)+1;
+
+        if l_child >= n {
+            break;
+        }
+
+        let r_child = l_child+1;
+
+        let max_child = if (r_child >= n) || (seq[l_child] >= seq[r_child]) {
+            l_child
+        } else {
+            r_child
+        };
+
+        if root >= seq[max_child] {
+            break;
+        }
+
+        seq[idx] = seq[max_child];
+        idx = max_child;
+    }
+
+    if idx != start {
+        seq[idx] = root;
     }
 }
--- tests/test_heap.rs
+++ tests/test_heap.rs
@@ -1,3 +1,6 @@
+use rand::distributions::{Distribution, Uniform};
+use rand::thread_rng;
+
 use basics::sorting::{max_heapify};
 
 fn check_have_same_elements(l_seq: &[i32], r_seq: &[i32]) {
@@ -83,7 +86,7 @@ fn test_max_heapify_empty() {
 fn test_max_heapify_singleton() {
     let mut seq = [42];
     max_heapify(&mut seq);
-    assert_eq!(42, seq[0]);
+    assert_eq!([42], seq);
 }
 
 #[test]
@@ -115,7 +118,13 @@ fn test_max_heapify_pair() {
 #[test]
 fn test_max_heapify_depth_2() {
     let seqs = [
+        vec![42, 13, 43],
         vec![42, 43, 13],
+        vec![42, 42, 43],
+        vec![42, 43, 42],
+        vec![42, 43, 43],
+        vec![42, 88, 99],
+        vec![42, 99, 88],
     ];
 
     for seq in seqs {
@@ -126,9 +135,78 @@ fn test_max_heapify_depth_2() {
     }
 }
 
-// Multiple levels but goes down only a few
-// Multiple levels and goes down all the way
-// Not all nodes in a level have children
-// A node doesn't have all children
-// Not all leaves are on the same level
-// Random
+#[test]
+fn test_max_heapify_depth_3() {
+    let seqs = [
+        // Move one level down
+        vec![42, 55, 11, 14],
+        vec![42, 55, 11, 14, 17],
+        vec![42, 55, 11, 14, 17,  5],
+        vec![42, 55, 11, 14, 17,  5,  3],
+        vec![42, 11, 55,  5],
+        vec![42, 11, 55,  5,  3],
+        vec![42, 11, 55,  5,  3, 14],
+        vec![42, 11, 55,  5,  3, 14, 17],
+        // Move two levels down
+        vec![10, 55, 11, 14],
+        vec![10, 55, 11, 14, 17],
+        vec![10, 55, 11, 17, 14],
+        vec![ 3, 55, 11, 14, 17,  5],
+        vec![ 4, 55, 11, 14,  7,  5,  3],
+        vec![ 9, 11, 55,  5,  3, 14],
+        vec![16, 11, 55,  5,  3, 14, 17],
+    ];
+
+    for seq in seqs {
+        let mut seq_cp = seq.clone();
+        max_heapify(&mut seq_cp);
+        check_have_same_elements(&seq, &seq_cp);
+        check_is_max_heap(&seq_cp);
+    }
+}
+
+fn get_random_max_heap() -> Vec<i32> { // Minimum length is 3
+    let mut rng = thread_rng();
+    let deltas_range = Uniform::from(0 .. 1000);
+    let len = Uniform::from(3 .. 4000).sample(&mut rng);
+    let mut vec = Vec::<i32>::with_capacity(len);
+
+    vec.push(Uniform::from(5000..20000).sample(&mut rng));
+
+    for child in 1..len {
+        let parent = if child % 2 == 1 {
+            (child-1)/2
+        } else {
+            (child-2)/2
+        };
+
+        vec.push(vec[parent] - deltas_range.sample(&mut rng));
+    }
+
+    return vec;
+}
+
+#[test]
+fn test_max_heapify_random_already_heap() {
+    for _ in 0..10 {
+        let vec = get_random_max_heap();
+        let mut vec_cp = vec.clone();
+        max_heapify(&mut vec_cp);
+        assert_eq!(vec, vec_cp);
+    }
+}
+
+#[test]
+fn test_max_heapify_random() {
+    let mut rng = thread_rng();
+
+    for _ in 0..10 {
+        let mut vec = get_random_max_heap();
+        vec[0] = Uniform::from(-2000..std::cmp::max(vec[1], vec[2])).sample(&mut rng);
+
+        let mut vec_cp = vec.clone();
+        max_heapify(&mut vec_cp);
+        check_have_same_elements(&vec, &vec_cp);
+        check_is_max_heap(&vec_cp);
+    }
+}
