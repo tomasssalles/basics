d67d9742878c837bd8c4abbc36d4439e2423ab35
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-11-05 20:58:32 +0100

    Implemented basic "any occurrence" binary search.

--- src/sorting/binary_search.rs
+++ src/sorting/binary_search.rs
@@ -1,14 +1,21 @@
 pub fn binary_search(seq: &[i32], target: i32) -> (usize, usize) {
-    let n = seq.len();
+    let mut lb = 0;
+    let mut ub = seq.len();
 
-    if n == 1 {
-        if seq[0] == target {
-            return (0, 1);
+    while lb != ub {
+        let mid = lb + ((ub-lb) / 2);
+        let val = seq[mid];
+
+        if val == target {
+            return (mid, mid+1);
         }
-        if seq[0] < target {
-            return (1, 1);
+
+        if val < target {
+            lb = mid+1;
+        } else {
+            ub = mid;
         }
     }
 
-    return (0, 0);
+    return (lb, ub);
 }
