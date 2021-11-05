3cc5d022daf865a56593be80a62707f9b759b38b
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-11-05 19:36:08 +0100

    Finished tests for binary search.

--- src/sorting/binary_search.rs
+++ src/sorting/binary_search.rs
@@ -2,11 +2,11 @@ pub fn binary_search(seq: &[i32], target: i32) -> (usize, usize) {
     let n = seq.len();
 
     if n == 1 {
-        if seq[0] < target {
-            return (1, 1);
-        }
         if seq[0] == target {
             return (0, 1);
+        }
+        if seq[0] < target {
+            return (1, 1);
         }
     }
 
--- tests/test_binary_search.rs
+++ tests/test_binary_search.rs
@@ -2,21 +2,45 @@ use basics::sorting::binary_search;
 
 #[test]
 fn test_binary_search_empty() {
-    let res = binary_search(&[], 42);
-    assert_eq!(res, (0, 0));
+    assert_eq!(binary_search(&[], 42), (0, 0));
 }
 
 #[test]
 fn test_binary_search_singleton_missing() {
-    let res1 = binary_search(&[99], 42);
-    assert_eq!(res1, (0, 0));
-
-    let res2 = binary_search(&[7], 42);
-    assert_eq!(res2, (1, 1));
+    assert_eq!(binary_search(&[99], 42), (0, 0));
+    assert_eq!(binary_search(&[7], 42), (1, 1));
 }
 
 #[test]
 fn test_binary_search_singleton_present() {
-    let res = binary_search(&[42], 42);
-    assert_eq!(res, (0, 1));
+    assert_eq!(binary_search(&[42], 42), (0, 1));
+}
+
+#[test]
+fn test_binary_search_pair_missing() {
+    assert_eq!(binary_search(&[99, 101], 42), (0, 0));
+    assert_eq!(binary_search(&[7, 101], 42), (1, 1));
+    assert_eq!(binary_search(&[7, 23], 42), (2, 2));
+}
+
+#[test]
+fn test_binary_search_pair_present() {
+    assert_eq!(binary_search(&[42, 99], 42), (0, 1));
+    assert_eq!(binary_search(&[7, 42], 42), (1, 2));
+    assert_eq!(binary_search(&[42, 42], 42), (0, 2));
+}
+
+#[test]
+fn test_binary_search_large_missing() {
+    assert_eq!(binary_search(&[76, 76, 77, 78, 99, 101, 101], 42), (0, 0));
+    assert_eq!(binary_search(&[-7, 0, 0, 1, 101], 42), (4, 4));
+    assert_eq!(binary_search(&[7, 8, 13, 14, 15, 23, 23, 23], 42), (8, 8));
+}
+
+#[test]
+fn test_binary_search_large_present() {
+    assert_eq!(binary_search(&[42, 50, 51, 51, 60, 99], 42), (0, 1));
+    assert_eq!(binary_search(&[7, 9, 42, 42, 42, 42, 43, 88, 99], 42), (2, 6));
+    assert_eq!(binary_search(&[-3, -2, -1, 0, 1, 42, 42], 42), (5, 7));
+    assert_eq!(binary_search(&[42, 42, 42, 42, 42, 42, 42], 42), (0, 7));
 }
